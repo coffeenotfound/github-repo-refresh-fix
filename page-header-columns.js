3542e087c3f78e7404279e6bf7c2706732a5214a
Jan Katzer <code.katzer@gmail.com>
2020-07-10 16:38:46 +0200

    Create page-header-columns.js

---
+++ page-header-columns.js
@@ -0,0 +1,12 @@
+/*! This fix fixes the page header stretching across the entire screen
+ * and constrains it to the main page column width. */
+
+.js-header-wrapper {
+    background-color: #24292e;
+}
+
+.js-header-wrapper header {
+    max-width: 1280px;
+    margin: 0 auto;
+    background-color: transparent;
+}
